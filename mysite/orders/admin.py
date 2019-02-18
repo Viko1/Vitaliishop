a01810d838b5ca97accaf243e11b6f4f4a5a5baf
Vitalii <vitaliiropiak@gmail.com>
2019-02-18 22:59:28 +0100

    Adding test masage

--- mysite/orders/admin.py
+++ mysite/orders/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+#test 111222333
 # class SubscriberAdmin(admin.ModelAdmin):
 #     list_display = ["name", "email"]
 #     list_display = [field.name for field in Subscriber._meta.fields]
