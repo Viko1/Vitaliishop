6d8c866cb5be2eb690cfd63f570da5aa8b222a19
Vitalii <vitaliiropiak@gmail.com>
2019-02-19 21:57:15 +0100

    Add models of our shop

--- mysite/orders/models.py
+++ mysite/orders/models.py
@@ -4,12 +4,26 @@ class Order(models.Model):
     customer_email = models.EmailField(blank=True, null=True, default=None)
     customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)
     customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
+    comments = models.TextField(blank=True, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
         return "Order %s" % self.id
-
     class Meta:
         verbose_name = 'Order'
-        verbose_name = 'Orders'
+        verbose_name_plural = 'Orders'
+
+class ProductInOrder(models.Model):
+    order = models.ForeignKey(Order, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product, blank=True, null=True, default=None)
+
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "Product %s" % self.product.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
--- mysite/products/models.py
+++ mysite/products/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
-class Subscriber(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=128)
+class Product(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    description = models.TextField(blank=True, null=True, default=None)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "User %s %s" % (self.name, self.email,)
+        return "Order %s" % self.id
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
