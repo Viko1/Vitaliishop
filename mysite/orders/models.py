eacc0a5149096d94cf3a7347a1e224e38474ade0
Vitalii <vitaliiropiak@gmail.com>
2019-02-19 22:56:06 +0100

    Add models of our shop2

--- mysite/orders/models.py
+++ mysite/orders/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 
+import products.models
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=24, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "Status %s" % self.name
+    class Meta:
+        verbose_name = 'Status'
+        verbose_name_plural = 'Statuses'
+
 class Order(models.Model):
     customer_email = models.EmailField(blank=True, null=True, default=None)
     customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)
     customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
     comments = models.TextField(blank=True, null=True, default=None)
+    status = models.ForeignKey(Status)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "Order %s" % self.id
+        return "Order %s %s" % (self.id, self.status.name)
     class Meta:
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
@@ -17,7 +33,7 @@ class Order(models.Model):
 class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, blank=True, null=True, default=None)
     product = models.ForeignKey(Product, blank=True, null=True, default=None)
-
+    is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
