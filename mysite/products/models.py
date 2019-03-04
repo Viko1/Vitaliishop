598bf5660a8fe23ad7fa3715a39748ab25be53f8
Vitalii <vitaliiropiak@gmail.com>
2019-03-04 23:11:07 +0100

    tworzenie rozdziałów laptop i phones, dodawanie zniżki, oraz kategorii

--- mysite/products/models.py
+++ mysite/products/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "%s" % self.name
+
+    class Meta:
+        verbose_name = 'Category of product'
+        verbose_name_plural = 'Category of products'
+
+
+
 class Product(models.Model):
 
     name = models.CharField(max_length=64, blank=True, null=True, default=None)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    discount = models.IntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, blank=True, null=True, default=None, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True, default=None)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
