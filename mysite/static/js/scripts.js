4eb61e7e26edb275f941dd8354ac469438ecfb63
Vitalii <vitaliiropiak@gmail.com>
2019-03-12 22:58:01 +0100

    tworzenie strony produktu v2 and navbar

--- mysite/static/js/scripts.js
+++ mysite/static/js/scripts.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
         console.log('123');
         var nmb = $('#number').val();
         console.log(nmb);
-
+        var submit_btn = $('#submit_btn');
+        var product_id = submit_btn.data("product_id");
+        var product_name = submit_btn.data("name");
+        var product_price = submit_btn.data("price");
+        console.log(product_id);
+        console.log(product_name);
     })
 });
