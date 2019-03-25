122ca64d0da2ff51c0aa50d384bdcf571c22536a
Vitalii <vitaliiropiak@gmail.com>
2019-03-25 23:27:52 +0100

    tworzymy session key zeby pozniej polaczyc z ajax i zrobic nasz cart 5

--- mysite/static/js/scripts.js
+++ mysite/static/js/scripts.js
@@ -13,9 +13,7 @@ $(document).ready(function(){
         console.log(product_id);
         console.log(product_name);
 
-        $('.basket-items ul').append('<li>'+product_name+', ' + nmb + 'szt. ' + 'po ' + product_price + 'zł  ' +
-            '<a class="delete-item" href="">x</a>'+
-            '</li>');
+
 
         var data = {};
         data.product_id = product_id;
@@ -34,12 +32,19 @@ $(document).ready(function(){
                 console.log(data.products_total_nmb);
                 if (data.products_total_nmb){
                    $('#basket_total_nmb').text("(" + data.products_total_nmb+ ")");
+                    console.log(data.products);
+                    $('.basket-items ul').html("");
+                    $.each(data.products, function (k, v) {
+                        $('.basket-items ul').append('<li>'+ v.name+ ', ' + v.nmb + 'szt. ' + 'po ' + v.price_per_item + 'zł  ' +
+                            // '<a class="delete-item" href="">x</a>'+
+                            '</li>');
+                    });
                 }
 
             },
             error: function () {
                 console.log("error")
-                
+
             }
         })
 
