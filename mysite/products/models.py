b8087c9c4c8a46fb90e35f44da769e757b1999f0
Vitalii <vitaliiropiak@gmail.com>
2019-04-24 23:43:18 +0200

    styling strony

--- mysite/products/models.py
+++ mysite/products/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
 
     product = models.ForeignKey(Product, blank=True, null=True, default=None, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='static/media/')
+    image = models.ImageField(upload_to='static/media/product_images')
     is_main = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
